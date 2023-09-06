97d6e78716d1d2cdd48a8798ccb31fe2458d82bd
Michael O'Connell <michael.oconnell@teamwork.com>
2023-09-06 10:23:40 +0100

    add timeline placeholder

--- src/lib/components/timeline/TimelineTrack.svelte
+++ src/lib/components/timeline/TimelineTrack.svelte
@@ -1,37 +1,12 @@
 <script>
 	import TimelineTrackItem from './TimelineTrackItem.svelte';
-  let currentMouseXPosition;
-
-	// let moving = false;
-
-	// function onMouseDown() {
-	// 	console.log('down');
-	// 	moving = true;
-	// }
-
-	// function onMouseMove(e) {
-  //   currentMouseXPosition = e.movementX 
-	// 	if (moving) {
-	// 		left += e.movementX;
-	// 	}
-	// }
-
-	// function onMouseUp() {
-	// 	moving = false;
-	// }
+	let currentMouseXPosition;
 </script>
 
 <div class="timeline-track">
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
-	<div
-		class="timeline-track-item-wrapper"
-	>
-		<TimelineTrackItem
-      mouseX={currentMouseXPosition}
-    />
-	</div>
+	<TimelineTrackItem mouseX={currentMouseXPosition}/>
 </div>
-
 
 <style lang="scss">
 	.timeline-track {
@@ -39,10 +14,6 @@
 		width: 100%;
 		height: 30px;
 		overflow: hidden;
+		display: flex;
 	}
-  .timeline-track-item-wrapper {
-    user-select: none;
-		cursor: move;
-		position: absolute;
-  }
 </style>
--- src/lib/components/timeline/TimelineTrackItem.svelte
+++ src/lib/components/timeline/TimelineTrackItem.svelte
@@ -1,23 +1,27 @@
 <script>
 	export let text = 'Hello World';
 	export let width = 200;
-	export let left = 100;
-
+	export let left = 100;	
+	
 	let moving = false;
 	let selectedHandle;
+	const placeholderPadding = 5;
+	let placeholderWidth = left - placeholderPadding;
 
 	function onMouseMove(e) {
 		if (moving) {
+			const x = e.movementX;
 			if (selectedHandle === 'r') {
-				width += e.movementX;
+				width += x;
 				return;
 			}
 			if (selectedHandle === 'l') {
-				left += e.movementX;
-				width -= e.movementX;
-				return;
+				width -= x;
 			}
-      left += e.movementX;
+			const newLeft = left + x;
+			if (newLeft <= 0) { return; }
+			left = newLeft;
+			placeholderWidth = left - placeholderPadding;
 		}
 	}
 
@@ -32,6 +36,7 @@
 	};
 </script>
 
+<div class="timeline-track__placeholder" style="width: {placeholderWidth}px;"/>
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
 	class="timeline-track-item"
@@ -50,10 +55,15 @@
 <svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
 
 <style lang="scss">
+	.timeline-track__placeholder {
+		background-color: #dddddf14;
+		height: 100%;
+	}
+
 	.timeline-track-item {
     position: absolute;
 		display: flex;
-		height: 25px;
+		height: 100%;
 		min-width: 1px;
 		cursor: grab;
 		align-items: center;
@@ -63,13 +73,16 @@
 	.timeline-track-item-handle {
 		z-index: 1;
 		height: 100%;
-		width: 8px;
+		width: 4px;
+		flex-shrink: 0;
 		top: 0;
 		cursor: ew-resize;
 		background-color: yellow;
 	}
 
 	.timeline-track-item-text {
-		padding: 8px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
 	}
 </style>
