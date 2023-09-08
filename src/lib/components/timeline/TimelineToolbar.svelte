b1a78cfd0f64639d0d171a9b15b128006a3a81c0
Michael O'Connell <michael.oconnell@teamwork.com>
2023-09-08 17:05:54 +0100

    add divider

--- src/lib/components/timeline/TimelineToolbar.svelte
+++ src/lib/components/timeline/TimelineToolbar.svelte
@@ -7,4 +7,20 @@
   <button on:click={() => dispatch('zoom', 'in')}>+</button>
   <button on:click={() => dispatch('zoom', 'out')}>-</button>
   <button on:click={() => dispatch('resetZoom')}>Reset</button>
+  <div class="timeline-toolbar__divider"/>
+  <button on:click={() => dispatch('split')}>Split</button>
 </div>
+
+<style lang="scss">
+  .timeline-toolbar {
+    display: flex;
+    align-items: center;
+  }
+  .timeline-toolbar__divider {
+    content: "";
+    width: 1px;
+    height: 10px;
+    background: rgba(255,255,255,.6);
+    margin: 0 8px;
+  }
+</style>
