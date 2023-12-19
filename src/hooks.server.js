638fe1f06515ca16fb1ef7e4b4aa8d02d49cda4c
MichaelWebCork <m.oconnell56@yahoo.ie>
2023-12-19 21:14:20 +0000

    trying more shit

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -36,6 +36,12 @@ export const handle = async ({ event, resolve }) => {
 			return name === 'content-range';
 		}
 	});
+	response.headers.append('Access-Control-Allow-Credentials', 'true');
 	response.headers.append('Access-Control-Allow-Origin', '*');
+	response.headers.append('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+	response.headers.append(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	);
 	return response;
 };
