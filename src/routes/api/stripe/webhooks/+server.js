ba2fdca8632bebb939ab8039551f79b2fe39ba6a
MichaelWebCork <m.oconnell56@yahoo.ie>
2023-12-21 02:13:08 +0000

    simplify

--- src/routes/api/stripe/webhooks/+server.js
+++ src/routes/api/stripe/webhooks/+server.js
@@ -1,82 +1,90 @@
-// in src/routes/stripe/webhooks/+server.js
-import { stripe } from '$lib/server/stripe';
-import { error, json } from '@sveltejs/kit';
-// import { env } from '$env/dynamic/private';
+// // in src/routes/stripe/webhooks/+server.js
+// import { stripe } from '$lib/server/stripe';
+// import { error, json } from '@sveltejs/kit';
+// // import { env } from '$env/dynamic/private';
 
-export const config = {
-	runtime: 'edge'
-};
+// export const config = {
+// 	runtime: 'edge'
+// };
 
-// endpoint to handle incoming webhooks
-export async function POST({ request }) {
-	console.log('Im hit');
-	// extract body
-	const body = await request.text();
+// // endpoint to handle incoming webhooks
+// export async function POST({ request }) {
+// 	console.log('Im hit');
+// 	// extract body
+// 	const body = await request.text();
 
-	// get the signature from the header
-	const signature = request.headers.get('stripe-signature');
+// 	// get the signature from the header
+// 	const signature = request.headers.get('stripe-signature');
 
-	// var to hold event data
-	let event;
+// 	// var to hold event data
+// 	let event;
 
-	// verify the signature matches the body
-	try {
-		// event = stripe.webhooks.constructEvent(body, signature, env.STRIPE_WEBHOOK_SIGNING_SECRET);
-		event = stripe.webhooks.constructEvent(
-			body,
-			signature,
-			'whsec_NghENbaXa3Xa8oQZhnv7irKpyiOkYj6I'
-		);
-	} catch (err) {
-		// warn when signature is invalid
-		console.warn('⚠️  Webhook signature verification failed.', err.message);
+// 	// verify the signature matches the body
+// 	try {
+// 		// event = stripe.webhooks.constructEvent(body, signature, env.STRIPE_WEBHOOK_SIGNING_SECRET);
+// 		event = stripe.webhooks.constructEvent(
+// 			body,
+// 			signature,
+// 			'whsec_NghENbaXa3Xa8oQZhnv7irKpyiOkYj6I'
+// 		);
+// 	} catch (err) {
+// 		// warn when signature is invalid
+// 		console.warn('⚠️  Webhook signature verification failed.', err.message);
 
-		// return, because signature is invalid
-		error(400, 'Invalid request');
-	}
+// 		// return, because signature is invalid
+// 		error(400, 'Invalid request');
+// 	}
 
-	/* Signature has been verified, so we can process events
-	 *
-	 * Review important events for Billing webhooks:
-	 * https://stripe.com/docs/billing/webhooks
-	 */
-	console.log('event.type: ', event.type);
+// 	/* Signature has been verified, so we can process events
+// 	 *
+// 	 * Review important events for Billing webhooks:
+// 	 * https://stripe.com/docs/billing/webhooks
+// 	 */
+// 	console.log('event.type: ', event.type);
 
-	switch (event.type) {
-		case 'checkout.session.completed':
-			// checkout complete
-			break;
-		case 'customer.subscription.created':
-			// Subscription was created
-			// Note: status will be `incomplete`
-			break;
-		case 'customer.subscription.updated':
-			// Subscription has been changed
-			break;
-		case 'invoice.paid':
-			// Used to provision services after the trial has ended.
-			// The status of the invoice will show up as paid. Store the status in your
-			// database to reference when a user accesses your service to avoid hitting rate limits.
-			break;
-		case 'invoice.payment_failed':
-			// If the payment fails or the customer does not have a valid payment method,
-			//  an invoice.payment_failed event is sent, the subscription becomes past_due.
-			// Use this webhook to notify your user that their payment has
-			// failed and to retrieve new card details.
-			break;
-		case 'customer.subscription.deleted':
-			if (event.request != null) {
-				// handle a subscription canceled by your request
-				// from above.
-			} else {
-				// handle subscription canceled automatically based
-				// upon your subscription settings.
-			}
-			break;
-		default:
-		// Unexpected event type
-	}
+// 	switch (event.type) {
+// 		case 'checkout.session.completed':
+// 			// checkout complete
+// 			break;
+// 		case 'customer.subscription.created':
+// 			// Subscription was created
+// 			// Note: status will be `incomplete`
+// 			break;
+// 		case 'customer.subscription.updated':
+// 			// Subscription has been changed
+// 			break;
+// 		case 'invoice.paid':
+// 			// Used to provision services after the trial has ended.
+// 			// The status of the invoice will show up as paid. Store the status in your
+// 			// database to reference when a user accesses your service to avoid hitting rate limits.
+// 			break;
+// 		case 'invoice.payment_failed':
+// 			// If the payment fails or the customer does not have a valid payment method,
+// 			//  an invoice.payment_failed event is sent, the subscription becomes past_due.
+// 			// Use this webhook to notify your user that their payment has
+// 			// failed and to retrieve new card details.
+// 			break;
+// 		case 'customer.subscription.deleted':
+// 			if (event.request != null) {
+// 				// handle a subscription canceled by your request
+// 				// from above.
+// 			} else {
+// 				// handle subscription canceled automatically based
+// 				// upon your subscription settings.
+// 			}
+// 			break;
+// 		default:
+// 		// Unexpected event type
+// 	}
 
-	// return a 200 with an empty JSON response
-	return json();
+// 	// return a 200 with an empty JSON response
+// 	return json();
+// }
+
+// import { stripe } from '$lib/server/stripe';
+import { json } from '@sveltejs/kit';
+
+export async function POST() {
+	console.log('im hit!');
+	return json({ hello: 'from vercel' });
 }
